e9934257b4fdc766d5a7772290e77b3d7aecade2
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-07 17:08:27 +0600

    for loop

--- looping.py
+++ looping.py
@@ -1,6 +1,18 @@
+""" While loop
 x=0
 while x<=5:
     print(x)
     x =x+1
 print('Hello')
+"""
+
+"""for loop"""
+for i in range(0,10,2):
+    print(i)
+print('................')
+
+"""negative increment"""
+for i in range(5, -5, -2):
+    print(i)
+
 
