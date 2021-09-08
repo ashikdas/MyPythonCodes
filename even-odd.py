2aa9e161ae8c0ee06f499f76ff54b7f47dfa0980
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-08 21:18:05 +0600

    even odd number checking

---
+++ even-odd.py
@@ -0,0 +1,11 @@
+n = int(input('Enter your number: '))
+
+
+def check_even_odd(n):
+    if n % 2 == 0:
+        print('The number is even')
+    else:
+        print('The number is odd')
+
+
+check_even_odd(n)
