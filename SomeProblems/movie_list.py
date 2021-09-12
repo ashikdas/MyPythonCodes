488ed98def5205db6241bc29a35cc74d4a429e14
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-12 23:08:21 +0600

    not in and multiple value print

--- SomeProblems/movie_list.py
+++ SomeProblems/movie_list.py
@@ -1,15 +1,18 @@
 fav_movie = []
 
 while True:
-    print('Movie No. ' + str(len(fav_movie)+1) + ' or press ENTER to stop adding to the list.')
+    print('Movie No. ' + str(len(fav_movie) + 1) + ' or press ENTER to stop adding to the list.')
     movie = input()
 
     if movie == '':
-       break
+        break
 
     fav_movie = fav_movie + [movie]
 
 if len(fav_movie) != 0:
     print('The lists are: ')
     for i in range(len(fav_movie)):
-        print(str(i+1) + '. ' + fav_movie[i])
+        print(str(i + 1) + '. ' + fav_movie[i])
+
+else:
+    print('You don not have any movie list')
---
+++ in_notin.py
@@ -0,0 +1,18 @@
+# in and not in keywords
+"""
+my_tech = ['iphone', 'android', 'ASUS laptop', 'monitor']
+
+print('Enter a tech name: ')
+name = input()
+if name not in my_tech:
+    print('Nope its not in the list')
+else:
+    print(name+' is my tech')
+"""
+# Assuming multiple values at once
+
+chocolate_milk_shake = ['chocolate', 'milk', 'ice-cream', 'blender']
+x, y, z, q = chocolate_milk_shake
+print(x, y, z, q)
+
+
