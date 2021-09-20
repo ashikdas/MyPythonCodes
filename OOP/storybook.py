acf754ae780e28669587341a112dbacafa7f5fe1
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-21 02:37:16 +0600

    class method, static method added

--- OOP/storybook.py
+++ OOP/storybook.py
@@ -23,9 +23,33 @@ class StoryBook:
     def get_author_info(self):
         print(f'The Author name is: {self.author_name}, born: {self.author_born}')
 
-    # set discount
+    # applying discount to an instance
     def apply_discount(self):
         self.price = int(self.price - self.price * self.discount)
+
+    # Method to change price
+    def set_price(self, new_price):
+        self.price = new_price
+
+    # Class Method 1
+    @classmethod
+    def set_discount(cls, new_discount):
+        cls.discount = new_discount
+
+    # Class Method 2
+    @classmethod
+    def construct_from_string(cls, book_str):
+        name, price, author_name, author_born, pages = book_str.split(',')
+
+        return cls(name, price, author_name, author_born, pages)
+
+    # Static Methods
+    @staticmethod
+    def is_new(publishing_year):
+        if publishing_year > 2016:
+            print('Yes it is a new book!')
+        else:
+            print('No it is not!')
 
 
 # creating an instance/ object of the StoryBook Class
@@ -40,7 +64,19 @@ StoryBook.get_author_info(book_1)
 # print(book_1.no_of_books)
 # print(book_2.no_of_books)
 # print(StoryBook.no_of_books)
-print(book_2.price)
-book_2.discount = 0.3
-book_2.apply_discount()
-print(book_2.price)
+# print(book_2.price)
+# book_2.discount = 0.3
+# book_2.apply_discount()
+# print(book_2.price)
+
+# print(book_1.price)
+# print(book_1.discount)
+# StoryBook.set_discount(0.6)
+# print(book_1.apply_discount())
+# print(book_1.price)
+
+book_str = 'Harry Potter, 200, JK Rowling, 1970, 400'
+harry_potter = StoryBook.construct_from_string(book_str)
+print(harry_potter.name)
+
+StoryBook.is_new(book_1.publishing_year)
