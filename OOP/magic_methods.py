26f1b36c6114c37e6b99dcf8bc1dd3cf8427a84c
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-29 00:26:40 +0600

    magic method --add--, --sub-- override

--- OOP/magic_methods.py
+++ OOP/magic_methods.py
@@ -10,11 +10,26 @@ class Point2D:
         return f'Class: Point2D,' \
             f' x: {self.x}, y: {self.y}'
 
+    def __add__(self, other):
+        if isinstance(self, other.__class__):
+            return Point2D(self.x + other.x, self.y + other.y)
+        else:
+            return None
+
+    def __sub__(self, other):
+        if isinstance(self, other.__class__):
+            return Point2D(self.x - other.x, self.y - other.y)
+        else:
+            return None
 
 p1 = Point2D(2, 3)
 p2 = Point2D(4, 5)
+#
+# print(p1)
+#
+# print(repr(p2))
+# print(str(p2))
 
-print(p1)
-
-print(repr(p2))
-print(str(p2))
+p3 = p1+p2
+print(p3)
+print(p2-p1)
