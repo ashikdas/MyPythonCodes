ea5d3504477a999937dd59b6fbb999d08ed30557
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-30 01:45:26 +0600

    client class added

--- OOP/OOP_Project/mobile_tech.py
+++ OOP/OOP_Project/mobile_tech.py
@@ -12,5 +12,4 @@ class Mobile(Tech):
 
     def __str__(self):
         return f'Name: {self.name}\n Price: {self.price}\n Weight: {self.weight}\n' \
-               f'Color: {self.color}\n Screen: {self.screen}\n Camera: {self.camera}'
-    
+               f'Color: {self.color}\n Screen: {self.screen}\n Camera: {self.camera}\n'
