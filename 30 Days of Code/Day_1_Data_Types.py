31e7947991fbda215c9089c37c9217fef7d829ed
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-10-04 12:19:18 +0600

    Day-1 solve. 30 days of codes -hackerrank problem solve

---
+++ 30 Days of Code/Day_1_Data_Types.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+int_var = int(input())
+double_var = float(input())
+string_var = input()
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+int_var)
+
+# Print the sum of the double variables on a new line.
+print(d+double_var)
+
+# Concatenate and print the String variables on a new line
+print(s+string_var)
+# The 's' variable above should be printed first.
