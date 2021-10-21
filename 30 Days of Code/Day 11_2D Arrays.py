e7908a426877de13615345d6077eb01d8139af49
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-10-21 12:36:45 +0600

    Day 11: 2D Arrays -hackerrank 30 days of code

---
+++ 30 Days of Code/Day 11_2D Arrays.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    def get_hourglass_sum(matrix, row, col):
+        sum = 0
+        sum += matrix[row - 1][col - 1]
+        sum += matrix[row - 1][col]
+        sum += matrix[row - 1][col + 1]
+        sum += matrix[row][col]
+        sum += matrix[row + 1][col - 1]
+        sum += matrix[row + 1][col]
+        sum += matrix[row + 1][col + 1]
+        return sum
+
+
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    max_hourglass_sum = -63
+    for i in range(1, 5):
+        for j in range(1, 5):
+            current_hourglass_sum = get_hourglass_sum(arr, i, j)
+            if current_hourglass_sum > max_hourglass_sum:
+                max_hourglass_sum = current_hourglass_sum
+    print(max_hourglass_sum)
