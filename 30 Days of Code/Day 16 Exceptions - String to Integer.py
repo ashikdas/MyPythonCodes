5eee603a20c42166780d922e010f2b0d988eea33
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-10-24 19:58:01 +0600

    Day 16: Exceptions - String to Integer -hackerrank 30 days of code

---
+++ 30 Days of Code/Day 16 Exceptions - String to Integer.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    S = input()
+    try:
+        s = int(S)
+        print(s)
+    except:
+        print("Bad String")
